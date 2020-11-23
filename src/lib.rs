dba61109721b216e929bc540ef6fa4aafc1714ce
nrayamajhee <nishan@rayamajhee.com>
2020-11-23 16:18:07 -0600

    Add build script.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use wasm_bindgen::JsValue;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen(start)]
+#[wasm_bindgen]
 pub async fn run() -> Result<(), JsValue> {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
