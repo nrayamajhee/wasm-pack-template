5f1f35065c4e4646ce698366bfa23dcb81d3a5c2
nrayamajhee <nishan@rayamajhee.com>
2020-11-27 15:50:21 -0600

    Add rayon, worker pool, and improve the start function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
+#[macro_export]
+macro_rules! console_log {
+    ($($t:tt)*) => (crate::log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+extern "C" {
+    /// External binding to the js console.log method. You probably want to use console_log!() instead
+    /// of this.
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+    #[wasm_bindgen]
+    pub fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn logv(x: &JsValue);
+}
+
+mod pool;
 mod start;
+pub use pool::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
@@ -7,6 +26,54 @@ use wasm_bindgen::JsValue;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use crate::WorkerPool;
+use rayon::ThreadPool;
+use std::sync::Arc;
+
+pub struct RayonWorkers {
+    concurrency: usize,
+    worker_pool: WorkerPool,
+    rayon_pool: Arc<ThreadPool>,
+}
+
+impl RayonWorkers {
+    pub fn new(concurrency: Option<usize>) -> Self {
+        let concurrency = if let Some(con) = concurrency {
+            con
+        } else {
+            if let Some(window) = web_sys::window() {
+                window.navigator().hardware_concurrency() as usize
+            } else {
+                crate::log("Couldn't get window!");
+                2
+            }
+        };
+        let worker_pool = WorkerPool::new(concurrency).unwrap();
+        let rayon_pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(concurrency)
+            .spawn_handler(|thread| Ok(worker_pool.run(|| thread.run()).unwrap()))
+            .build()
+            .unwrap();
+        let rayon_pool = Arc::new(rayon_pool);
+        Self {
+            concurrency,
+            worker_pool,
+            rayon_pool,
+        }
+    }
+    pub fn concurrency(&self) -> usize {
+        self.concurrency
+    }
+    pub fn run<F: 'static + Fn() + Sync + Send>(&self, closure: F) {
+        let r_p = self.rayon_pool.clone();
+        self.worker_pool.run(move || {
+            r_p.install(|| {
+                closure();
+            });
+        });
+    }
+}
 
 #[wasm_bindgen]
 pub async fn run() -> Result<(), JsValue> {
--- src/start.rs
+++ src/start.rs
@@ -1,13 +1,21 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+use crate::RayonWorkers;
+use rayon::{prelude::*, ThreadPool};
 
 pub async fn start() -> Result<(), JsValue> {
-    alert("Hello from Rust via WASM!");
+    // Generate rayon thread-pool with the number of threads
+    // given by window.navigator.hardwareConcurency
+    let workers = RayonWorkers::new(None);
+    // Spawn another web worker to run the multithreaded
+    // part because blocking is not allowed in the main thread
+    let num = workers.concurrency();
+    workers.run(move || {
+        (1..=num as i32).into_par_iter().for_each(|e| {
+            crate::logv(&JsValue::from(format!("Hey from thread number {}!", e)));
+        });
+    });
+    crate::alert("Hey from main thread!\nCheck the dev console for messages from other threads.");
     Ok(())
 }
-
